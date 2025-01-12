0990d65b1aa7013b7af6eaa4b2699684252e4f31
Vadim K. <vadim@vadirn.io>
2025-01-12 14:11:08 +0500

    feat: source mode after creation

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,7 @@ export default class NewFilePlugin extends Plugin {
             const newFile = await this.createFile(activeFile, newFileName);
             const leaf = this.app.workspace.getLeaf('tab');
             await leaf.openFile(newFile);
+            await leaf.setEphemeralState({ mode: 'source' });
             new Notice(`Created new file: ${newFileName}`);
         } catch (error) {
             console.error('Error creating new file:', error);
